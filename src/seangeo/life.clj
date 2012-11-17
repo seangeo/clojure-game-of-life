15d5a926d7bfd7a933f3b46582d5397893b0018d
Sean Geoghegan <sean@seangeo.me>
2012-11-17 20:30:02 +1100

    Got Life simulation working

--- src/seangeo/life.clj
+++ src/seangeo/life.clj
@@ -1,10 +1,66 @@
 (ns seangeo.life
-  (:gen-class))
+  (:gen-class)
+  (use clojure.set))
 
-(load "model")
+(load "ui")
+
+(defrecord Automaton [x y])
+
+(defn world
+  [live-cells]
+  (set (map #(apply ->Automaton %1) live-cells)))
+
+(defn neighbours
+  [automaton]
+  (let
+    [cx (:x automaton)
+     cy (:y automaton)
+     xs [(- cx 1) cx (+ cx 1)]
+     ys [(- cy 1) cy (+ cy 1)]]
+    (for [x xs y ys 
+          :when (not (and (= x cx) (= y cy)))] 
+      [x y])))
+
+(defn alive?
+  [automaton my-world]
+  (contains? my-world automaton))
+
+(defn dead?
+  [automaton my-world]
+  (not (contains? my-world automaton)))
+
+(defn enough-to-survive?
+  [live-neighbours]
+  (or (= 2 live-neighbours) (= 3 live-neighbours)))
+
+(defn survivor
+  [cell my-world]
+  (let
+    [automaton     (apply ->Automaton cell)
+     my-neighbours (world (neighbours automaton))
+     live-neighbours (count (intersection my-world my-neighbours))]
+    (if (and (alive? automaton my-world) (enough-to-survive? live-neighbours)) #{automaton} #{})))
+
+(defn birth
+  [cell my-world]
+  (let
+    [potential-life (apply ->Automaton cell)
+     my-neighbours (world (neighbours potential-life))
+     potential-parents (count (intersection my-world my-neighbours))]
+     (if (and (= potential-parents 3) (dead? potential-life my-world)) 
+       #{potential-life} #{})))
+
+(defn world-cells [width height]
+  (for [x (range width) y (range height)] [x y]))
+
+(defn evolve-cell [old-world new-world cell]
+  (union new-world (birth cell old-world) (survivor cell old-world)))
+
+(defn evolve-world
+  [current-world width height]
+  (reduce (partial evolve-cell current-world) #{} (world-cells width height)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-ui))
 
--- test/seangeo/life_test.clj
+++ test/seangeo/life_test.clj
@@ -2,6 +2,96 @@
   (:use clojure.test
         seangeo.life))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest world-builder
+  (testing "The builder of the worlds"
+    (is (=
+          (set [(->Automaton 1 1)])
+          (world [[1 1]])))))
+
+(deftest find-neighbours
+  (testing "Finding neighbour cells"
+    (is (= 
+          (set
+            [[0 0] [1 0] [2 0]
+             [0 1]       [2 1]
+             [0 2] [1 2] [2 2]])
+          (set (neighbours (->Automaton 1 1)))))))
+
+(deftest test-survival-1
+   (testing "Automatons should survive if they have 3 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 1] [1 0] [2 0] [1 1]])]
+        (is (= #{(->Automaton 1 1)} (survivor automaton my-world)))))
+
+(deftest test-survival-2
+   (testing "Automatons should survive if they have 2 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 1] [2 0] [1 1]])]
+        (is (= #{(->Automaton 1 1)} (survivor automaton my-world)))))
+
+(deftest test-survival-1-when-dead
+   (testing "Dead automatons shouldn't comeback if they have 3 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 1] [1 0] [2 0]])]
+        (is (= #{} (survivor automaton my-world)))))
+
+(deftest test-survival-2-when-dead
+   (testing "Dead automatons shouldn't come back to life if they have 2 neighbours, this is handled by birthing")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 1] [2 0]])]
+        (is (= #{} (survivor automaton my-world)))))
+(deftest test-survival-3
+   (testing "Automatons should die if they have 4 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 0] [0 1] [0 2] [1 2] [1 1]])]
+        (is (= #{} (survivor automaton my-world)))))
+
+(deftest test-survival-4
+   (testing "Automatons should die if they have 1 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[0 0] [1 1]])]
+        (is (= #{} (survivor automaton my-world)))))
+
+(deftest test-survival-5
+   (testing "Automatons should die if they have 0 neighbours")
+      (let
+        [automaton '(1 1)
+         my-world (world [[1 1]])]
+        (is (= #{} (survivor automaton my-world)))))
+
+(deftest test-birth-1
+   (testing "Automatons should be born if there are 3 neighbours")
+      (let
+        [cell '(1 1)
+         my-world (world [[0 0] [1 0] [1 2]])]
+        (is (= #{(->Automaton 1 1)} (birth cell my-world)))))
+
+(deftest test-birth-2
+   (testing "Automatons should not be born if there is 2 neighbours")
+      (let
+        [cell '(1 1)
+         my-world (world [[1 0] [1 2]])]
+        (is (= #{} (birth cell my-world)))))
+
+(deftest test-birth-3
+   (testing "Automatons should not be born if there is 4 neighbours")
+      (let
+        [cell '(1 1)
+         my-world (world [[1 0] [0 1] [0 2] [2 2]])]
+        (is (= #{} (birth cell my-world)))))
+
+(deftest test-evolve-world
+   (testing "Test that the correct next frame is produced"
+      (let
+        [my-world (world [[1 0] [1 1] [1 2]])
+         expected-world (world [[0 1] [1 1] [2 1]])
+         width    4
+         height   4]
+        (is (= expected-world (evolve-world my-world width height))))))
+
