a50032f3a06cb8915157e5a69ccec078763f3334
Sean Geoghegan <sean@seangeo.me>
2012-11-18 00:03:44 +1100

    Remove crap

--- src/seangeo/model.clj
+++
@@ -1,2 +0,0 @@
-(in-ns 'seangeo.life)
-
--- test/seangeo/life_model_test.clj
+++
@@ -1,7 +0,0 @@
-(ns seangeo.life-model-test
-  (:use clojure.test
-        seangeo.life))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
